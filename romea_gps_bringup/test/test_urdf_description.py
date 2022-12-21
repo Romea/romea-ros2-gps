6ba5e494129c22691504637be6145f9c805586fb
jean.laneurit <jean.laneurit@inrae.fr>
2022-12-21 17:41:44 +0100

    update urdf_description unit test

--- romea_gps_bringup/test/test_urdf_description.py
+++ romea_gps_bringup/test/test_urdf_description.py
@@ -1,24 +1,46 @@
-
 import os
 import pytest
+import subprocess
 
 from romea_gps_bringup import urdf_description
 
+from ament_index_python import get_package_prefix
+
 import xml.etree.ElementTree as ET
+
 
 @pytest.fixture(scope="module")
 def urdf():
-    meta_description_filename = os.path.join(os.getcwd(),"test_gps_bringup.yaml")
-    return ET.fromstring(urdf_description("robot_",meta_description_filename))
+
+    exe = (
+        get_package_prefix("romea_gps_bringup")
+        + "/lib/romea_gps_bringup/urdf_description.py"
+    )
+
+    meta_description_filename = os.path.join(os.getcwd(), "test_gps_bringup.yaml")
+
+    return ET.fromstring(
+        subprocess.check_output(
+            [
+                exe,
+                "robot_namespace:robot",
+                "meta_description_filename:" + meta_description_filename,
+            ],
+            encoding="utf-8",
+        )
+    )
 
 def test_gps_name(urdf):
-    assert urdf.find("link").get("name")=="robot_gps_link"
+    assert urdf.find("link").get("name") == "robot_gps_link"
+
 
 def test_gps_position(urdf):
-    assert urdf.find("joint/origin").get("xyz")=="1.0 2.0 3.0"
+    assert urdf.find("joint/origin").get("xyz") == "1.0 2.0 3.0"
+
 
 def test_gps_parent_link(urdf):
-    assert urdf.find("joint/parent").get("link")=="robot_base_link"
+    assert urdf.find("joint/parent").get("link") == "robot_base_link"
+
 
 def test_gps_rate(urdf):
     assert urdf.find("gazebo/sensor/update_rate").text == "10"
