877d3d031a0b1abfce0b19bc6793b45b6cc493ce
Jean Laneurit <jean.laneurit@inrae.fr>
2023-12-19 17:07:44 +0100

    description: add gps_simulation xacro file
    Signed-off-by: Jean Laneurit <jean.laneurit@inrae.fr>

--- romea_gps_description/python/romea_gps_description.py
+++ romea_gps_description/python/romea_gps_description.py
@@ -57,6 +57,9 @@ def urdf(prefix, mode, name, type, model, rate, dual_antenna, parent_link, xyz, 
 
     xacro_file = get_package_share_directory("romea_gps_description") + "/urdf/gps.xacro.urdf"
 
+    if mode == "simulation":
+       mode += "_gazebo_classic" 
+
     urdf_xml = xacro.process_file(
         xacro_file,
         mappings={
